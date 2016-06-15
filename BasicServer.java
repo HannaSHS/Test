94c898bae323ddec91e27e76f565b640f85c1550
jpleachon <jp.leachon30@gmail.com>
2016-06-16 03:20:56 +0800

    Added 200 and 404 responses for HEAD
    Respond with 404 when file does not exist,
    Respond with 200 when request is succesful

--- BasicServer.java
+++ BasicServer.java
@@ -83,6 +83,54 @@ public class BasicServer implements Runnable {
     }
   }
 
+  private void doHead(HttpRequest request, HttpResponse response) throws IOException{
+  // HEAD request
+
+     File file = new File(BASE_PATH + request.uri);
+
+    if (!file.exists()) {
+      not_found(response);
+      return;
+    }
+
+    response.status = "200 OK";
+    response.headers.put("Content-Length", String.valueOf(file.length()));
+
+    if (request.uri.endsWith(".css")) {
+        response.headers.put("Content-Type", "text/css");
+        //not_found(response);
+        //return;
+      } else if (request.uri.endsWith(".html")) {
+        response.headers.put("Content-Type", "text/html");
+      } else if (request.uri.endsWith(".js")) {
+        response.headers.put("Content-Type", "text/javascript");
+        //not_found(response);
+        //return;
+      } else if (request.uri.endsWith(".json")) {
+        response.headers.put("Content-Type", "application/json");
+      } else if (request.uri.endsWith(".xml")) {
+        response.headers.put("Content-Type", "text/xml");
+      }  else {
+        response.headers.put("Content-Type", "text/plain");
+      }
+
+    response.sendHeaders();
+    response.writer.close();
+    return;
+  }
+
+  private void doPost(HttpRequest request, HttpResponse response) throws IOException{
+  // POST request
+  }
+  
+  private void doPut(HttpRequest request, HttpResponse response) throws IOException{
+  // PUT request
+  }
+  
+  private void doDelete(HttpRequest request, HttpResponse response) throws IOException{
+  // DELETE request
+  }
+
   private void not_found(HttpResponse response) throws IOException {
     response.status = "404 Not Found";
 
@@ -95,7 +143,13 @@ public class BasicServer implements Runnable {
     if (request.isGet()) {
       doGet(request, response);
     } else if (request.isHead()) {
-      // doHead(request, response);
+        doHead(request, response);
+    } else if (request.isPost()) {
+      // doPost(request, response);
+    } else if (request.isPut()) {
+      // doPut(request, response);
+    } else if (request.isDelete()) {
+      // doDelete(request, response);
     }
 
     System.out.println();
